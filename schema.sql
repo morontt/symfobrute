a6326d95711066162da0667dd0036bed561ca12e
morontt <morontt@gmail.com>
2015-12-10 21:54:33 +0200

    upgrade schema

--- schema.sql
+++ schema.sql
@@ -3,6 +3,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `salt` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `checked` TINYINT NOT NULL DEFAULT 0,
   `plain_password` VARCHAR(255) NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
